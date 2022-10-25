939533714356d02f388d9b34619311e8796a9c5c
Jose4580 <113045496+Jose4580@users.noreply.github.com>
2022-10-25 11:39:37 +0530

    Add files via upload

---
+++ Syhn.java
@@ -0,0 +1,18 @@
+class Syhn
+{
+   Synchronized void printTable(int n)
+   {
+     for(int i=1;i<=5;i++)
+     {
+       System.out.println(n*i);
+     }
+     try
+     {
+       Thread.sleep(500);
+      }
+      catch(Exception e)
+      {
+        System.out.println(e);
+       }
+    }
+}
